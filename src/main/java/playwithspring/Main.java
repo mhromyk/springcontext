926a8c858132f9cc4ca171f3230ca3417d6945d3
Maks <mhromyk.edu@gmail.com>
2023-11-27 19:40:24 +0100

    initial code before AOP

--- src/main/java/playwithspring/Main.java
+++ src/main/java/playwithspring/Main.java
@@ -3,26 +3,17 @@ package playwithspring;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import playwithspring.config.ProjectConfig;
 import playwithspring.model.Comment;
-import playwithspring.proxy.EmailCommentNotificationProxy;
-import playwithspring.repository.InMemoryCommentRepository;
 import playwithspring.service.CommentService;
 
 public class Main {
 
-    public static void main(String[] args){
-
-//        var commentRepository = new InMemoryCommentRepository();
-//        var commentNotificationProxy = new EmailCommentNotificationProxy();
-//
-//        var commentService = new CommentService(commentNotificationProxy, commentRepository);
+    public static void main(String[] args) {
 
         var comment = new Comment("Maks", "This is first comment");
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         var commentService1 = context.getBean(CommentService.class);
-        var commentService2 = context.getBean(CommentService.class);
 
         commentService1.publishComment(comment);
-        System.out.println("Instance of commentService1 is equal to commentService2 : " + (commentService1 == commentService2));
     }
 }
